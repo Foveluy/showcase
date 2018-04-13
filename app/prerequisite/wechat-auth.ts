a5e02b1a14af61d162449fb4f2f03f5e459b9bfc
Z F <215566435@qq.com>
2018-04-13 12:01:44 +1200

    undefind

--- app/prerequisite/wechat-auth.ts
+++ app/prerequisite/wechat-auth.ts
@@ -3,18 +3,15 @@ import { Context } from 'egg'
 export function WechatAuth(ctx: Context, ctl: any) {
     try {
         var token = ctx.request.headers['authorization']
-        console.log(token)
         if (token) {
             token = token.split(' ')[1]
         } else {
+            ctx.logger.info('token = undefind')
             token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MjA1NjY5Mjh9.OP8r2-asdasda'
         }
 
-    
         const obj = ctx.app.jwt.verify(token, ctx.app.config.secret)
-
         ctx.ticket = obj['token']
-
         ctx.logger.info('login_ok')
     } catch (e) {
         ctl.ResponseJson({
